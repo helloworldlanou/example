e0dd40967e074c8fb38d9e845adc5c2a8dcc15f7
wangz <junyi_882@hotmail.com>
2012-11-06 09:12:14 +0800

    ok

--- JavaTests/src/example/algorithm/Knap.java
+++ JavaTests/src/example/algorithm/Knap.java
@@ -3,11 +3,13 @@ public class Knap {
 	/**
 	 * 0/1背包问题。all 为所有物件。max为包的容量。要求拿出的物件加起来正好是包的重量。
 	 * 这个算法只能算出一种结果。不能列出所有可能的组合。
+	 * 此算法不能包含到所有情况，而且总是会包含第一个！（这算法还需要改进）
 	 * @param args
 	 */
 	private int[] all;
 	private int max;
 	public boolean knapprocess(int m,int n) {//m是剩余量，n是当前第几个
+		System.out.println(m+" "+n);
 		boolean result;
 		int x = m-all[n];//x为包含了第n个时候的剩余容量
 		if(x==0){
@@ -39,7 +41,7 @@ public class Knap {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Knap knap = new Knap();
-		knap.all = new int[]{1,2,3,4,5,6,7,8,9,10};
+		knap.all = new int[]{10,9,8,7,6,5,4,3,2,1};
 		knap.max = 21;
 		System.out.println(knap.knapprocess(knap.max,knap.all.length-1));
 	}
