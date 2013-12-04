5ccdade300218cc6f4c5fbcee2bbcafc9f074f0e
wangz <wangzhuo1987@gmail.com>
2013-12-04 11:45:31 +0800

    66 Change-Id: Iae80f62289a866dbd83319eadc9dbbc95d9137f7

--- JavaTests/src/exam/sjcp/q66/ClassC.java
+++ JavaTests/src/exam/sjcp/q66/ClassC.java
@@ -1,4 +1,8 @@
 package exam.sjcp.q66;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class ClassA{}
 class ClassB extends ClassA{}
 public class ClassC extends ClassA{
@@ -10,10 +14,19 @@ public class ClassC extends ClassA{
 		ClassA p4 = new ClassC();
 		
 		p0 = p1;
-		p1 = p2;
-		p2 = p4;
-		p2 = (ClassC)p1;
+//		p1 = p2;
+//		p2 = p4;
+//		p2 = (ClassC)p1;
 		p1 = (ClassB)p3;
 		p2 = (ClassC)p4;
+		
+		Map<String, String> map = new HashMap();
+		map.put(null, "a");
+		map.put(null, "b");
+		map.put("", "c");
+		map.put("", "d");
+		System.out.print(map.containsKey(null));
+		System.out.print(map.get(null));
+		System.out.print(map.get(""));
 	}
 }
