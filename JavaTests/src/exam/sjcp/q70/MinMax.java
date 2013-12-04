faa241a9d999440dd3a1326a5dd63b9ec0e26415
wangz <wangzhuo1987@gmail.com>
2013-12-04 13:45:59 +0800

    q
    Change-Id: I561c4e46c10a412a812000be274729ef82f73a4d

---
+++ JavaTests/src/exam/sjcp/q70/MinMax.java
@@ -0,0 +1,32 @@
+package exam.sjcp.q70;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.codec.binary.StringUtils;
+import org.jsoup.helper.StringUtil;
+
+//public class MinMax <N extends Number>{
+public class MinMax <N extends Integer>{
+//public class MinMax <? extends Number>{//不合法 通配符只能用于方法中？？
+	private N min,max;
+	public N getMin(){ return min; }
+	public N getMax(){ return max; }
+	public void add(N added){
+		if(min == null || added.doubleValue() < min.doubleValue()){
+			min = added;
+		}
+		if(max == null || added.doubleValue() > max.doubleValue()){
+			max = added;
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		List<int> a = new ArrayList();
+
+	}
+
+}
